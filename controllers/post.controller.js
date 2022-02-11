67f1f3a35b858a8e66adffeaa386ee474f0cd359
Thibaud <thibaud.marti@orange.fr>
2022-02-11 17:05:09 +0100

    Add post.model

---
+++ controllers/post.controller.js
@@ -0,0 +1,8 @@
+const PostModel = require("../models/post.model");
+const UserModel = require("../models/user.model");
+const ObjectID = require("mongoose").Types.ObjectId;
+
+exports.readPost = (req, res) => {};
+exports.createPost = (req, res) => {};
+exports.updatePost = (req, res) => {};
+exports.deletePost = (req, res) => {};
